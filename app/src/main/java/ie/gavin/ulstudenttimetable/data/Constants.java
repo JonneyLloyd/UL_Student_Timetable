4514eff564a21834463ced60010c446a9b15ace5
OliverGavin <14161044@studentmail.ul.ie>
2016-04-11 23:32:21 +0100

    Broadcast constants

---
+++ app/src/main/java/ie/gavin/ulstudenttimetable/data/Constants.java
@@ -0,0 +1,14 @@
+package ie.gavin.ulstudenttimetable.data;
+
+/**
+ * Created by Oliver on 29/03/2016.
+ */
+public class Constants {
+    // Defines custom Intent action
+    public static final String BROADCAST_ACTION =
+            "ie.gavin.oliver.ulstudenttimetable.BROADCAST";
+
+    // Defines the key for the status "extra" in an Intent
+    public static final String EXTENDED_DATA_STATUS =
+            "ie.gavin.oliver.ulstudenttimetable.STATUS";
+}
