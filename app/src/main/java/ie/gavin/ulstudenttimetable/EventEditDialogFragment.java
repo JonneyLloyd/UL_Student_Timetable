b99b637c0a5a4a46cf7bcb81c0c10c2bb250968a
OliverGavin <14161044@studentmail.ul.ie>
2016-04-25 23:33:14 +0100

    Basic view/edit dialog implementation with the necessary callbacks and data paths

---
+++ app/src/main/java/ie/gavin/ulstudenttimetable/EventEditDialogFragment.java
@@ -0,0 +1,60 @@
+package ie.gavin.ulstudenttimetable;
+
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+
+import ie.gavin.ulstudenttimetable.data.StudentTimetable;
+
+public class EventEditDialogFragment extends EventDialogFragment {
+
+    private StudentTimetable studentTimetable;
+//    private EditText mEditText;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
+        studentTimetable = super.getStudentTimetable();
+
+        View view = inflater.inflate(R.layout.fragment_edit_event, container, false);
+
+//        TextView tv = (TextView) view.findViewById(R.id.text);
+//        tv.setText("This is an instance of ActionBarDialog edit");
+
+//        mEditText = (EditText) view.findViewById(R.id.username);
+
+        Toolbar toolbar = (Toolbar) view.findViewById(R.id.toolbar);
+        toolbar.setOnMenuItemClickListener(new Toolbar.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem item) {
+                // Handle the menu item
+                int id = item.getItemId();
+
+                if (id == R.id.action_cancel) {
+                    // Return to activity
+                    closeEventDialogListener activity = (closeEventDialogListener) getActivity();
+                    activity.onCloseEventDialog(CANCEL_ACTION, studentTimetable);
+                    dismiss();
+                    return true;
+                } else if (id == R.id.action_save) {
+                    // Return to activity
+                    closeEventDialogListener activity = (closeEventDialogListener) getActivity();
+                    activity.onCloseEventDialog(SAVE_ACTION, studentTimetable);
+                    dismiss();
+                    return true;
+                }
+
+                return true;
+            }
+        });
+        toolbar.inflateMenu(R.menu.edit_event);
+        toolbar.setTitle(studentTimetable.get_moduleCode());
+
+        return view;
+
+    }
+
+}
